cc377b24c5f5ae55fbf7b0b2b7c378d3a9f3ee16
ZZSECCIAC <claudio.seccia@vodafone.com>
2026-05-04 12:48:11 +0200

    feat: ex1 use props in components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,27 @@ import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
 
 function App() {
+  const menu = [
+    { name: "Home", link: "#" },
+    { name: "Contatti", link: "#" },
+  ];
+  const textLogo = "Logo";
+  const posts = [
+    {
+      title: "Post 1",
+      text: "Testo di esempio per la prima card...",
+    },
+    {
+      title: "Post 2",
+      text: "Testo di esempio per la seconda card...",
+    },
+    {
+      title: "Post 3",
+      text: "Testo di esempio per la terza card...",
+    },
+  ];
+  console.log(posts, textLogo, menu);
+  //usare map per iterare i post e renderizzare una card per ogni post
   return (
     <div className="app">
       <Navbar />
